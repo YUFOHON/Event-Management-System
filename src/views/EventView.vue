93b05aa69cb4f02d1b5a8c865986046d4a4cc87e
Yu Fo Hon <yukehan8@gmail.com>
2023-02-07 12:24:03 +0800

    b

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,48 +1,50 @@
 <template>
 
 
-        <div class="row">
-            <navBar />
-        </div>
-        <div class="row py-4">
-            <navSecondBar :arr="[
-            {
-                name: '主頁',
-                URL: '/'
-            }
-            ,
-            {
-                name: '活動',
-                URL: 'events'
-            }
-            ,
-            {
-                name: 'Veronica',
-                URL: 'Veridian Dynamics'
-            }]" :sortButton="true" :eventHistoryButton="true" :addButton="true" :search="true" />
+    <div class="row">
+        <navBar />
+    </div>
+    <div class="row py-4">
+        <navSecondBar :arr="[
+        {
+            name: '主頁',
+            URL: '/'
+        }
+        ,
+        {
+            name: '活動',
+            URL: 'events'
+        }
+        ,
+        {
+            name: 'Veronica',
+            URL: 'Veridian Dynamics'
+        }]" :sortButton="true" :eventHistoryButton="true" :addButton="true" :search="true" />
+    </div>
+
+    <div class="row">
+        <div v-for="index in 8" :key="index" class="col p">
+            <eventCard />
         </div>
 
-        <div class="row">
-            <eventForm />
-        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 import navBar from '@/components/public/navBar.vue'
-// import SideBar from '@/components/sideBar.vue';
 import navSecondBar from '@/components/Bruce/navSecondBar.vue'
-
-import eventForm from '@/components/Bruce/eventForm.vue'
-
+import eventCard from '@/components/Bruce/eventCard.vue';
 export default {
     name: 'EventView',
     components: {
         navBar,
         navSecondBar,
-        eventForm
-        // SideBar
-    }
+        eventCard
+
+    },
+
+
 }
 </script>
