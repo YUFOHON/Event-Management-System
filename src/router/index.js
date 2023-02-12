aefbcc6745b28dfbb1f74f0943ffb3649f9614ff
wty <21219443@life.hkbu.edu.hk>
2023-02-13 03:20:06 +0800

    finished client profile and update function

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,13 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import EventView from '../views/Bruce/EventView.vue'
 import EventFormView from '../views/Bruce/EventFormView.vue'
+import AdminProfile from '../views/AdminProfile.vue'
+import ClientProfile from '../views/ClientProfile.vue'
+import EditAdminProfile from '../views/EditAdminProfile.vue'
+import EditClientProfile from '../views/EditClientProfile.vue'
+import cEnrollment from '../views/cEnrollment.vue'
+
+
 const routes = [
   {//=================================Bruce==========================================================
     path: '/',
@@ -20,6 +27,33 @@ const routes = [
     component: EventFormView
   },
 
+  //=====================================JOHN=============================================================
+  {
+    path: '/aProfile',
+    name: 'AdminProfile',
+    component: AdminProfile
+  },
+  {
+    path: '/cProfile',
+    name: 'ClienbtProfile',
+    component: ClientProfile
+  },
+  {
+    path: '/editAProfile/:id/',
+    name: 'editAdminProfile',
+    component: EditAdminProfile
+  },
+  {
+    path: '/editCProfile/:id/',
+    name: 'editClientProfile',
+    component: EditClientProfile
+  },  
+  {
+    path: '/cEnrollment/',
+    name: 'clientEnrollment',
+    component: cEnrollment
+  },
+  
   //==================================================================================================
   //==================================================================================================
   {
---
+++ src/views/AdminProfile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+
+    <div class="row">
+      <navBar />
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import navBar from '@/components/public/navBar.vue'
+// import navSecondBar from '@/components/Bruce/navSecondBar.vue'
+
+// import SideBar from '@/components/sideBar.vue';
+// import navSecondBar from '@/components/Bruce/navSecondBar.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    navBar,
+    // navSecondBar
+    // SideBar
+  }
+}
+</script>
