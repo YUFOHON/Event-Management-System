1f5e894feea82f65fec6c8a4d2ba61bac34856e3
TamYeeLam <20223471@life.hkbu.edu.hk>
2023-03-19 10:44:59 +0800

    Feedback

--- src/views/feedBackView.vue
+++ src/views/feedBackView.vue
@@ -2,28 +2,17 @@
     <div class="row" id="navBar">
         <navBar />
     </div>
-    <div class="row col-5">
-        <h4 class="fw-bold text-center mt-3"></h4>
+    <div class="container">
         <form @submit.prevent="submitSurvey()">
+            <h1> 活動名稱 </h1>
             <div class="row" v-for="q in questions" :key="q">
-                <p class="fw-bold">{{ q }}</p>
+                <h5 class="fw-bold">{{ q }}</h5>
                 <div class="form-check mb-2">
-                    <input class="form-check-input" type="radio" name="exampleForm" id="radioExample1" />
-                    <label class="form-check-label" for="radioExample1">
-                        Option 1
-                    </label>
-                </div>
-                <div class="form-check mb-2">
-                    <input class="form-check-input" type="radio" name="exampleForm" id="radioExample2" />
-                    <label class="form-check-label" for="radioExample2">
-                        Option 2
-                    </label>
-                </div>
-                <div class="form-check mb-2">
-                    <input class="form-check-input" type="radio" name="exampleForm" id="radioExample3" />
-                    <label class="form-check-label" for="radioExample3">
-                        Option 3
-                    </label>
+                    <input type="radio" id="one" value="One" />
+                    <label for="one">option 1</label>
+
+                    <input type="radio" id="two" value="Two" />
+                    <label for="two">option 2</label>
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
@@ -34,7 +23,7 @@
 <script>
 // import { VueFeedbackReaction } from 'vue-feedback-reaction';
 // import { VueReactionEmoji } from 'vue-feedback-reaction';
-import { ref } from "vue";
+import { ref, onMounted, computed } from "vue";
 import navBar from '@/components/public/navBar.vue';
 
 export default {
@@ -44,7 +33,11 @@ export default {
         // VueFeedbackReaction,
         // VueReactionEmoji
     },
-    setup() {
+    props: {
+        eventName: String,
+        image: String,
+    },
+    setup(props) {
         const feedback = ref({});
         const questions = ["活動日期、時間、地點合適",
             "活動內容達到你 / 你孩子的期望",
@@ -56,15 +49,42 @@ export default {
             "活動能增加你對 CCF 的歸屬感",
             "工作員能清晰講解及指導活動進行",
             "您滿意活動的整體效果"]
+        const fileFormat = ref('')
+        const url = ref(props.image)
+        const textColor = ref('#000000')
+        const defaultColor = computed(() => {
+            if (props.Category == '兒童活動') {
+                return '#af8221'
+            } else if (props.Category == '青年活動') {
+                return '#037316'
+            } else if (props.Category == '同路人支援平台') {
+                return '#8a56dd'
+            } else {
+                return '#ff0000'
+            }
+        })
 
         const submitSurvey = async function () {
 
         }
 
+        onMounted(() => {
+            textColor.value = defaultColor.value
+            // console.log(props.id)
+            if (props.file == undefined) {
+                url.value = 'default'
+            } else {
+                fileFormat.value = props.file[0].split("/")[1].split(";")[0]
+                // console.log(fileFormat.value)
+                url.value = '/api/files/' + props.id + '.' + fileFormat.value
+            }
+        })
+
         return {
             navBar,
             questions,
             feedback,
+            props,
             submitSurvey
         }
     }
@@ -76,3 +96,16 @@ export default {
     // })
 };
 </script>
+
+<style> 
+.container {
+     display: flex;
+     align-items: center;
+     gap: 50px;
+     background-color: rgb(226, 217, 220);
+     border-radius: 10px;
+ }
+ input[type="radio"] {
+  margin: 10px;
+}
+</style>
